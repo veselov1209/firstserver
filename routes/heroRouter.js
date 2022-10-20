e55fb12e97e514690497459fbdaadf9cc4dce193
veselov1209 <veselov1209@gmail.com>
2022-10-20 15:57:17 +0300

    routes

--- routes/heroRouter.js
+++ routes/heroRouter.js
@@ -1,5 +1,9 @@
 const {Router} = require('express');
+const {heroController} = require('../controllers')
+
 
 const heroRouter = Router();
+
+heroRouter.route('/').get()
 
 module.exports = heroRouter;
--- routes/powerRouter.js
+++ routes/powerRouter.js
@@ -1,5 +1,8 @@
 const {Router} = require('express');
+const {powerController} = require('../controllers');
 
 const powerRouter = Router();
+
+// powerRouter.route('/').get()
 
 module.exports = powerRouter;
